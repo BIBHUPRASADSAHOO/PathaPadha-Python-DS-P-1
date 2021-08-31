b96f14e0b0743421fe267e43cdb4b29db9ecfa42
BIBHUPRASADSAHOO <89767385+BIBHUPRASADSAHOO@users.noreply.github.com>
2021-08-31 17:39:32 +0530

    swap two number

---
+++ Assignment-1(swap two number).py
@@ -0,0 +1,11 @@
+x=11
+y=25
+print ("before swapping:")
+print ("value of x : " ,x,"and value of y : ",y)
+# code to swap 'x' and 'y'
+x,y=y,x
+print ("After swaping")
+print ("value of x : ",x," and value of y :",y)
+
+
+
